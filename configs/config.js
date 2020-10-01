ace1b686721650698b5d608d15973e3f5b5b39d5
deebindal <43185824+deebindal@users.noreply.github.com>
2020-10-01 21:42:05 +0530

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -1,6 +1,7 @@
 module.exports={
-    DB:'mongodb+srv://deebindal:PAHMb13C7RWPTajs@cluster0-szjv9.mongodb.net/test?retryWrites=true&w=majority',
-    API_KEY: 'e802dea8',
-    API_SECRET: 'XySCf2gSsuJeB05a',
-    KEY:'qwertyuiopasdfghjklzxcvbnm'
+    DB:DB_LINK,
+    API_KEY: API_KEY,
+    API_SECRET: API_SECRET,
+    KEY:KEY
 }
+//insert your database credentials here.
